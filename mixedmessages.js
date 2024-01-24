fa2d78947505f549534336d6e4b5714c4eddd9b5
Matt <matt_campbell@hotmail.co.uk>
2024-01-24 19:37:46 +0700

    completed code

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,5 +1,14 @@
-const MessageStart = "You are"
 
-const MessageMiddle = ["feeling", "needing"]
+const messageMiddle = ["should", "must", "will"]
 
-const messageEnd = ["lucky", ]
+const messageEnd = ["be brave.", "smile.", "take a chance.", "take a second to reflect."]
+
+//console.log(messageStart, messageMiddle[2], messageEnd[0])
+
+var rMiddle = messageMiddle[Math.floor(Math.random()*messageMiddle.length)];
+
+var rEnd = messageEnd[Math.floor(Math.random()*messageEnd.length)];
+
+let text = ("You" + " " + rMiddle + " " + rEnd);
+
+console.log(text)
